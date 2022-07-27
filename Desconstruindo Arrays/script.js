d5f077fd752280c55303c57bf984281ce67fe26b
Samuel Campos <scbrasilbr@gmail.com>
2022-07-27 14:08:20 -0300

    first commit

---
+++ Desconstruindo Arrays/script.js
@@ -0,0 +1,38 @@
+
+
+// 
+
+let pessoa = {
+    nome: "Samuel",
+    sobrenome: "Campos",
+    empresa: "Sujeito Programador",
+    cargo: "Programador FullStack"
+  };
+  
+  //console.log(pessoa.nome);
+  //console.log(pessoa.cargo);
+  
+  // let nome = "TESTE";
+  
+  // const { nome:nomePessoa, cargo, empresa, sobrenome} = pessoa;
+  
+  // console.log("NOME DA PESSOA " + nomePessoa);
+  // console.log(sobrenome);
+  
+  // console.log(empresa);
+  // console.log(cargo);
+  
+  
+  // =================================
+  
+  let nomes = ["Samuel", "Lucas", "Henrique"];
+  
+  // let { 0:matheus, 2:terceiraPessoa } = nomes;
+  
+  // console.log(samuel);
+  // console.log(terceiraPessoa);
+  
+  let [primeironome, segundonome] = nomes;
+  
+  console.log(primeironome);
+  console.log(segundonome);
