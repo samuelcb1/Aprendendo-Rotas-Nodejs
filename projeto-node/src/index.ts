1bb761411a71c8190295e53736dd2e0a3e9d86fc
Samuel Campos <samuel.brasil@csacademy.com.br>
2023-01-06 08:54:33 -0300

    Adicioando o typescript e incrementando a froma de desenvover

--- projeto-node/src/index.ts
+++ projeto-node/src/index.ts
@@ -1,2 +1,14 @@
 
-console.log('Executei!');
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'sucesso total' })
+});
+
+app.listen(3000, () => {
+    console.log("aplicação executando na porta 3000");
+    
+})
+
